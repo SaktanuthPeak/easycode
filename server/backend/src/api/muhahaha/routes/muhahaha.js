00bce8f09029a96899ed7dfd89443cb91729360e
Kontuch Suksawat <zeroxr20@gmail.com>
2025-01-09 22:18:46 +0700

    test: strapi

---
+++ server/backend/src/api/muhahaha/routes/muhahaha.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * muhahaha router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::muhahaha.muhahaha');
---
+++ server/backend/src/api/muhahaha/services/muhahaha.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * muhahaha service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::muhahaha.muhahaha');
