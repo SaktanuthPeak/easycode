5375cb6ab99db8b7e8ce8324c33f9e4fef4d8515
SaktanuthPeak <thanakrit.5122548@gmail.com>
2025-02-18 22:19:33 +0700

    fix:navbar

--- client/src/teacher/component/teacherNav.js
+++ client/src/teacher/component/teacherNav.js
@@ -15,7 +15,7 @@ import logo from "./images/Logo2.png"
 const navigation = [
   { name: "Home", path: "/client-home", current: true },
   { name: "All courses", path: "/client-home/all-courses", current: false },
-  { name: "DashBoard", path: "/client-home/dashboard", current: false },
+  { name: "Dashboard", path: "/client-home/dashboard", current: false },
 ];
 
 function classNames(...classes) {
@@ -86,27 +86,7 @@ export default function TeacherNavBar() {
             </div>
           </div>
           <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button
-              type="button"
-              onClick={handleOpenCart}
-              className="relative rounded-full bg-white p-1 text-black-400 hover:text-gray-1000 focus:outline-0 focus:ring-2 focus:ring-black "
-            >
-              <span className="sr-only">View cart</span>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                class="size-6"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                />
-              </svg>
-            </button>
+
 
             {/* Profile dropdown */}
             <Menu as="div" className="relative ml-3">
