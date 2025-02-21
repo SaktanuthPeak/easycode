81eb83e22d06146d3312cfa3187716cef2c6da42
6710110212 <6710110212@psu.ac.th>
2025-02-21 23:18:57 +0700

    first draft profilePage

--- client/src/component/navbarLogin.js
+++ client/src/component/navbarLogin.js
@@ -35,7 +35,7 @@ export default function NavbarLogin() {
     navigate("client-home/cart");
   };
   const handleOpenProfile = () => {
-    navigate("/profile");
+    navigate("/client-home/profile-page");
   };
   const handleHome = () => {
     navigate("/client-home")
